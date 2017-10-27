fb36cda71f6561eb07cfd9f8ee9daf96708a6adf
Justin Liu <justinliu2015@gmail.com>
2017-10-27 15:08:58 -0700

    cleaned up files

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,19 +1,9 @@
-var express = require('express');
-var bcrypt = require('bcrypt');
-var app = express();
-var bodyParser=require('body-parser');
-var router = express.Router();
-var authenticateController=require('./../controllers/authenticate-controller');
-var registerController=require('./../controllers/register-controller');
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(bodyParser.json());
-/* route to handle login and registration */
-app.post('/api/register',registerController.register);
-app.post('/api/authenticate',authenticateController.authenticate);
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-module.exports = router;
+/*
+* GET home page.
+*/
+ 
+exports.index = function(req, res){
+    var message = '';
+  res.render('index',{message: message});
+ 
+};
