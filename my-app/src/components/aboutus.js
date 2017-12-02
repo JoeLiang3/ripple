ec724f857b01e6f587d80bbfdebd190a86d32d4a
Justin Wu <jwu90@ucsc.edu>
2017-12-01 20:08:06 -0800

    Editted about us page

--- my-app/src/components/aboutus.js
+++ my-app/src/components/aboutus.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom'
+import CoverPhoto from '../aboutus.jpg';
 import '../aboutus.css';
 
 class AboutUs extends Component {
@@ -8,8 +9,14 @@ class AboutUs extends Component {
   render() {
     console.log('hi');
     return(
+      <div>
       <div className="about-us-Header">
         <h1> About Us </h1>
+      </div>
+
+      <div className="CoverPhoto">
+        <img src={CoverPhoto}  alt="" />
+      </div>
       </div>
     );
   }
