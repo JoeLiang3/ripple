25f420377d27d10afd6ee5e2e436fbe7680d2fa3
Justin Wu <jwu90@ucsc.edu>
2017-12-01 20:25:23 -0800

    changes to about us

--- my-app/src/components/aboutus.js
+++ my-app/src/components/aboutus.js
@@ -10,14 +10,21 @@ class AboutUs extends Component {
     console.log('hi');
     return(
       <div>
-      <div className="about-us-Header">
-        <h1> About Us </h1>
+        <div className="about-us-Header">
+          <h1> About Us </h1>
+        </div>
+
+        <div className="CoverPhoto">
+          <img src={CoverPhoto} height="500" width="900"  alt="" />
+        </div>
+
+        <div className="AboutUsBio">
+          Ripple is a web application that connects citizens and politicians, enabling both parties with access to current legislative information and a manageable communications and review system. Our mission is to build the next platform for the next generation to be more informed and involved in politics.
+        </div>
+
       </div>
 
-      <div className="CoverPhoto">
-        <img src={CoverPhoto}  alt="" />
-      </div>
-      </div>
+
     );
   }
 }
